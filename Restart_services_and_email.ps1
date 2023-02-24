102b6a5f039796da40d823c435d57bf3bed591eb
edbat <edwardbattye@gmail.com>
2023-02-24 11:11:41 +0000

    Create Restart_services_and_email.ps1

---
+++ Restart_services_and_email.ps1
@@ -0,0 +1,19 @@
+﻿#Use this script as a recovery action on a windows service
+$svc = Get-Service -Servicename 'Service_name_here'
+
+$svc | Set-Service -Status Running
+$svc.WaitForStatus('Running','00:01:00')
+
+#If services need restarting on other services you can do this here
+
+#Invoke-Command -ComputerName SVRATLAS001 -ScriptBlock {Restart-Service 'Atlas 6 Ax4 AX-live Service'}  
+
+$mailProps = @{
+    To = ''
+    From = ''
+    Subject = ''
+    Body = -join  '' | Out-String
+    SmtpServer = ''
+}
+
+Send-MailMessage @mailProps
